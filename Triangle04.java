9b0c2e9521b747e006225f4099f2b73022638403
akmaliarimahati <akmaliarimahati09@gmail.com>
2025-11-14 13:30:17 +0700

    modifikasi percobaan 3

--- Triangle04.java
+++ Triangle04.java
@@ -7,13 +7,14 @@ public class Triangle04 {
         System.out.print("Masukkan Nilai N= ");
         int n = sc.nextInt();
         int i = 0;
-        
+
         while (i <= n) {
             int j = 0;
             while (j < i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
         }
 
